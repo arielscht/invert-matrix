266e2b5ab4d605d09ae716d029a20fbbe1f8e8ad
Luis Felipe Risch <luis.risch@visagio.com>
2022-09-07 22:41:23 -0300

    feat: added python scripts

---
+++ commonConstants.py
@@ -0,0 +1,21 @@
+ROOT_LKD_COMMAND = 'likwid-perfctr'
+ROOT_CSV_DIR = 'csv'
+ROOT_LOG_DIR = 'logs'
+ROOT_PDF_DIR = 'pdfs'
+VERSIONS  = ['v1']
+MATRIX_SIZES = [32, 33, 64, 65, 128, 129, 256, 257, 512, 1000]
+INTERESTED_GROUPS = ["L3", "ICACHE", "FLOPS_DP"]
+INTERESTED_DATA = ["L3", "ICACHE", "FLOPS_DP", "TIME"]
+INTERESTED_PATTERNS = {
+  "L3": "(L3 bandwidth \[MBytes\/s] *\| *)([0-9]+\.?[0-9]*|\.[0-9]+)",
+  "ICACHE": "(L1I miss ratio *\| *)([0-9]+\.?[0-9]*|\.[0-9]+)",
+  "FLOPS_DP": "(  DP \[?MFLOP/s\]? *\| *)([0-9]+\.?[0-9]*|\.[0-9]+)",
+  "TIME": "(RDTSC Runtime \[s\] *\| *)([0-9]+\.?[0-9]*|\.[0-9]+)"
+}
+LABELS = {
+  "L3": "L3 bandwidth MBytes/s",
+  "ICACHE": "L1I miss ratio",
+  "FLOPS_DP": "DP MFLOP/s",
+  "TIME": "seconds"
+}
+
--- generateLogs.py
+++ generateLogs.py
@@ -1,37 +1,31 @@
 import os
-
-# constants
-ROOT_LKD_COMMAND = 'likwid-perfctr'
-ROOT_LOG_DIR = 'logs'
-VERSIONS  = ['v1']
-MATRIX_SIZES = [32, 33, 64, 65, 128, 129, 256, 257, 512, 1000, 2000, 4000, 6000, 10000]
-INTERESTED_GROUPS = ["L3", "ICACHE", "FLOPS_DP"]
+import commonConstants as constants
 
 os.system('make')
 
 def handleExistenceNecessaryDirs():
-  logPath = f'./{ROOT_LOG_DIR}'
+  logPath = f'./{constants.ROOT_LOG_DIR}'
   if(not os.path.exists(logPath)):
     os.mkdir(logPath)
 
-  for version in VERSIONS:
+  for version in constants.VERSIONS:
     versionPath = os.path.join(logPath, version)
     if(not os.path.exists(versionPath)):
       os.mkdir(versionPath)
 
 def buildLogPath(size, version, group):
-  return f'./{ROOT_LOG_DIR}/{str(version)}/{str(group)}{"-"}{str(size)}.log'
+  return f'./{constants.ROOT_LOG_DIR}/{str(version)}/{str(group)}{"-"}{str(size)}.log'
 
 def buildProgramPath(size, itr = 10):
   return f'./invmat -r {str(size)} -i {str(itr)} -s /dev/null'
 
 def buildLkdCommand(group, core = 3):
-  return f'{ROOT_LKD_COMMAND} -C {str(core)} -g {str(group)} -m'
+  return f'{constants.ROOT_LKD_COMMAND} -C {str(core)} -g {str(group)} -m'
 
 handleExistenceNecessaryDirs()
 
-for version in VERSIONS:
-  for size in MATRIX_SIZES:
-    for group in INTERESTED_GROUPS:
+for version in constants.VERSIONS:
+  for size in constants.MATRIX_SIZES:
+    for group in constants.INTERESTED_GROUPS:
       command = f'{buildLkdCommand(group)} {buildProgramPath(size)} {">"} {buildLogPath(size, version, group)}'
       os.system(command)
